ba1dd33d636def520596ceacf58a176a7dd2f0fa
bribbon <s.jordi@emubox.ch>
2021-03-20 14:13:37 +0100

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cd /home/container
+cd /home/pptruser
 
 # Output Current Java Version
 node -version ## only really needed to show what version is being used. Should be changed for different applications
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+echo ":/home/pptruser$ ${MODIFIED_STARTUP}"
 
 # Run the Server
 ${MODIFIED_STARTUP}
