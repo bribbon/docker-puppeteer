46b85805fc3ec5c27b197a8c1336d70eebc7cdd8
bribbon <s.jordi@emubox.ch>
2021-03-21 16:27:23 +0100

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 cd /home/container
 
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
 # Output Current Java Version
 node -v ## only really needed to show what version is being used. Should be changed for different applications
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
