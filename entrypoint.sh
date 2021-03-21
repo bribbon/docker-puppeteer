519cd1ec352ce31c50364b2171295e84f512bd9e
bribbon <s.jordi@emubox.ch>
2021-03-21 16:09:54 +0100

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
+eval ${MODIFIED_STARTUP}
